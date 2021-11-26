9b299708918e7cb0889a3a00efb97a9f056b7433
juancanas45 <84482151+juancanas45@users.noreply.github.com>
2021-11-26 14:15:49 -0500

    Update dias.js
    parte del codigo falta complñetar los meses y los dias

--- src/routes/dias.js
+++ src/routes/dias.js
@@ -5,23 +5,36 @@ const router = Router();
 const dias = require('../clientes.json');
 //console.log(clientes);
 
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+
 
 router.get('/dias', (req, res) => {
      var html = '<form action="/diasv" method="POST">'+
      '<input type="hidden" name="oculto" val="campo oculto pero no invisible!">'+
- '<div> <label for="nombre">Nombre Empleado: </label>'+
+ '<div> <label for="nombre">Nombre Completo: </label>'+
  '<input type="text" id="nombre" name="nombre"></div>'+
- '<div> <label for="dcto">Documento Empleado: </label>'+
- '<input type="text" id="documento" name="documento"></div>'+
+ '<div> <label for="dcto">Dia Nacimiento: </label>'+
+ '<input type="text" id="dian" name="dian"></div>'+
 
-'<div> <label for="sal">Valor Sueldo Mensual: </label>'+
- '<input type="text" id="sueldo" name="sueldo"></div>'+
+'<div> <label for="sal">Mes Nacimiento: </label>'+
+ '<input type="text" id="mesn" name="mesn"></div>'+
 
- '<div> <label for="dias">Dias trabajo en el mes: </label>'+
- '<input type="text" id="dias" name="dias"></div>'+
+ '<div> <label for="dias">Año Nacimiento: </label>'+
+ '<input type="text" id="anon" name="anon"></div>'+
+ 
+  '</br><label>Dia Actual:'+ day +'</label>'+       
+  '<label>Mes Actual:'+ month +'</label>'+ 
+  '<label>Mes Actual:'+ year +'</label>'+ 
 
- '<div> <label for="extras">Numero de Horas Extras Mes: </label>'+
- '<input type="text" id="extras" name="extras"></div>'+
 
 '<div>'+
 '<button type="submit">Enviar</button>'+
@@ -33,26 +46,26 @@ router.get('/dias', (req, res) => {
 
 router.post('/diasv', (req, res) => {
     var nemp= req.body.nombre;
-    var demp= req.body.documento;
-    var sal= req.body.sueldo;
-    var dias= req.body.dias;
-    var extras= req.body.extras;
+    var ndia= req.body.dian;
+    var nmes= req.body.mesn;
+    var nano= req.body.anon;
+    
 
     //calculos
-    var liqd=(sal/30)*dias;
-    var hex=(((sal/30)/8)*0.25)*extras;
-    var td=liqd+hex;
+   if (nano>year){
+   res.send("Error el Año de nacimiento no puede ser mayor al año actual"); 
+      }else {
+     
+  var tano=year-nano;         
+  var tmes=year-nano;          
+     
 
-    var msghtml='Señor Empleado :' + nemp + '</br>'+
-    'Con Identificacion :' + demp + '</br>'+
-    'Con Salario Mensual :' + sal + '</br>'+
-    '</br> Liquidacion </br>'+
-    'Dias Trabajados :' + dias + ' Salario :'+ liqd +' </br>'+
-    'Horas extras trabajadas :' + extras + ' Valor :'+ hex +' </br>'+
-    'Total devengado :'+ td+ ' </br>'+
-    ' <a href="/form2"> Probar de Nuevo </a>';
+    var msghtml='usted Señor :' + nemp + ' hoy tiene </br>'+
+    '</br>' + tano + ' Años '++' Meses '++' dias</br>'+
+    ' <a href="/dias"> Probar de Nuevo </a>';
     res.send(msghtml);  
-});
+     });
 
+    }
 
 module.exports = router;
